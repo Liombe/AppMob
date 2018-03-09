a6ba34e02a586b897aee9fe80915d72a7387a8fc
Liombe <lea.chaminadas@gmail.com>
2018-03-09 18:29:40 +0100

    (Neluth) Sign up + sign in + reset password

--- app/src/main/java/com/dubalais/android/SignupActivity.java
+++ app/src/main/java/com/dubalais/android/SignupActivity.java
@@ -1,35 +1,57 @@
 package com.dubalais.android;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
+import com.dubalais.android.models.Chore;
 import com.dubalais.android.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity implements OnClickListener{
 
     private EditText inputEmail, inputPassword;
-    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private Button btnSignUp2, btnSignUp, btnResetPassword;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
-    private DatabaseReference db;
+    private static DatabaseReference db;
+
+    private String typePersonne="";
 
     private static final int SWIPE_MIN_DISTANCE = 120;
     private static final int SWIPE_MAX_OFF_PATH = 250;
@@ -48,63 +70,21 @@ public class SignupActivity extends AppCompatActivity implements OnClickListener
 
         //initializing views
         btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        btnSignUp2 = (Button) findViewById(R.id.sign_up_button2);
         inputEmail = (EditText) findViewById(R.id.txtemail);
         inputPassword = (EditText) findViewById(R.id.password);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
-
-        btnResetPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
-            }
-        });
-
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String email = inputEmail.getText().toString().trim();
-                String password = inputPassword.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (password.length() < 6) {
-                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                progressBar.setVisibility(View.VISIBLE);
-                //create user
-                auth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                Toast.makeText(SignupActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
-                                progressBar.setVisibility(View.GONE);
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                if (!task.isSuccessful()) {
-                                    Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(),
-                                            Toast.LENGTH_SHORT).show();
-                                } else {
-                                    createNewUser(task.getResult().getUser());
-                                    startActivity(new Intent(SignupActivity.this, MainActivity.class));
-                                    finish();
-                                }
-                            }
-                        });
-
+                btnSignUP();
+            }
+        });
+        btnSignUp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnSignUP();
             }
         });
         // Gesture detection
@@ -114,6 +94,58 @@ public class SignupActivity extends AppCompatActivity implements OnClickListener
                 return gestureDetector.onTouchEvent(event);
             }
         };
+
+
+        PlaceholderFragment fragment = new PlaceholderFragment();
+
+        FragmentManager  fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.contain_tache, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+    private void btnSignUP(){
+        String email = inputEmail.getText().toString().trim();
+        String password = inputPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        //create user
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(SignupActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            createNewUser(task.getResult().getUser());
+                            startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
     }
 
     private void createNewUser(FirebaseUser userRegistered){
@@ -163,5 +195,84 @@ public class SignupActivity extends AppCompatActivity implements OnClickListener
     protected void onResume() {
         super.onResume();
         progressBar.setVisibility(View.GONE);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        RadioButton rdF = findViewById(R.id.fournisseur);
+        RadioButton rdD = findViewById(R.id.demandeur);;
+
+        switch(view.getId()) {
+            case R.id.fournisseur:
+                if (rdF.isChecked()) {
+                    typePersonne = "fournisseur";
+                    rdD.setChecked(false);
+                }
+                break;
+            case R.id.demandeur:
+                if (rdD.isChecked()) {
+                    typePersonne = "demandeur";
+                    rdF.setChecked(false);
+                }
+                break;
+
+        }
+    }
+
+    public void suiteClicked(View view){
+        ConstraintLayout cl = findViewById(R.id.inscr1);
+        ConstraintLayout cF = findViewById(R.id.incr_fourn);
+        ConstraintLayout cD = findViewById(R.id.incr_dem);
+
+        switch(typePersonne){
+            case "fournisseur":
+                cl.setVisibility(View.GONE);
+                cF.setVisibility(View.VISIBLE);
+                break;
+            case "demandeur":
+                cl.setVisibility(View.GONE);
+                cD.setVisibility(View.VISIBLE);
+                break;
+            case "":
+                Toast.makeText(getApplicationContext(), "Choisissez un type", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            //final ConstraintLayout cl = findViewById();
+            final View view = inflater.inflate(R.layout.activity_signup, container, false);
+            //View view = new View(SignupActivity.this, null);
+            final FrameLayout cl = view.findViewById(R.id.contain_tache);
+
+            db.child("chores").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot child: dataSnapshot.getChildren()){
+                        String chId = child.getKey();
+                        Chore ch = child.getValue(Chore.class);
+                        ch.uid=chId;
+                        CheckBox check = new CheckBox(view.getContext());
+                        String title = ch.title.substring(0,1).toUpperCase() + ch.title.substring(1);
+                        check.setId(Integer.parseInt(ch.uid));
+                        check.setText(title);
+                        check.setTextColor(ContextCompat.getColor(view.getContext(), R.color.grisClair));
+
+                        Log.d("ceciestuntag", "jedebug1");
+                        cl.addView(check);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            return view;
+        }
     }
 }
